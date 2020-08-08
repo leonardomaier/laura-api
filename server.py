77a8eb4afbe069c8e6b6a360f3e9bf27f939b756
Leonardo Maier <leonarmaier@gmail.com>
2020-08-08 20:31:19 -0300

    Adiciona rotas que retornam estudantes e cursos

--- server.py
+++ server.py
@@ -1,17 +1,70 @@
-from database import mongo_uri
-from flask import Flask
+from database import mongo_uri, collection_name
+from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
+from bson.json_util import dumps, loads, RELAXED_JSON_OPTIONS
+
+from utils import json_encode
+
+import json
 
 app = Flask(__name__)
 
 app.config['MONGO_URI'] = mongo_uri()
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.config['JSON_AS_ASCII'] = False
 
 mongo = PyMongo(app)
+
+collection = mongo.db[collection_name()]
 
 
 @app.route("/", methods=['GET'])
 def home():
     return "API is running"
+
+
+@app.route("/students", methods=['GET'])
+def get_students():
+
+    params = request.args
+
+    errors = []
+
+    if 'modalidade' not in params:
+        errors.append({'message': 'modalidade param is missing'})
+
+    if 'data_inicio' not in params:
+        errors.append({'message': 'data_inicio param is missing'})
+
+    if 'data_final' not in params:
+        errors.append({'message': 'data_final param is missing'})
+
+    if len(errors):
+        return json_encode({'errors': errors})
+
+    students = collection.find({
+        '$and': [
+            {'modalidade': {'$eq': params.get('modalidade')}},
+            {'data_inicio': {
+                '$gte': params.get('data_inicio'),
+                '$lt': params.get('data_final')
+            }}
+        ]
+    })
+
+    return json_encode(students)
+
+
+@app.route("/courses", methods=['GET'])
+def get_courses():
+
+    params = request.args
+
+    errors = []
+
+    courses = collection.distinct('curso', {'curso': {'$ne': None}})
+
+    return json_encode(courses)
 
 
 app.run()
